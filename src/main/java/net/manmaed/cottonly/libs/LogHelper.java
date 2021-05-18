7aec6d6983063b19a0d22aa1ec6a9187cd713558
manmaed <manmaed18@gmail.com>
2021-05-18 13:59:30 +0100

    Cotten -> Cotton

---
+++ src/main/java/net/manmaed/cottonly/libs/LogHelper.java
@@ -0,0 +1,29 @@
+package net.manmaed.cottonly.libs;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by manmaed on 16/05/2021.
+ */
+public class LogHelper {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private static void log(Level loglvl, Object object) {
+        LOGGER.log(loglvl, "[" + loglvl + "]:" + "-" + RefHelper.LOG + " " + String.valueOf(object));
+    }
+
+    public static void info(Object object) {
+        log(Level.INFO, object);
+    }
+    public static void warn(Object object) {
+        log(Level.WARN, object);
+    }
+    public static void fatal(Object object) {
+        log(Level.FATAL, object);
+    }
+    public static void debug(Object object) {
+        log(Level.FATAL,object);
+    }
+}
