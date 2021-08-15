436932c78671315620e1aea62d77c2f6d2db70ae
manmaed <manmaed18@gmail.com>
2021-08-15 14:43:12 +0100

    Make it load config

--- src/main/java/net/manmaed/cottonly/Cottonly.java
+++ src/main/java/net/manmaed/cottonly/Cottonly.java
@@ -1,11 +1,15 @@
 package net.manmaed.cottonly;
 
+import net.manmaed.cottonly.config.CottonConfig;
 import net.manmaed.cottonly.libs.RefHelper;
 import net.manmaed.cottonly.libs.RegisterHandler;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -31,7 +35,11 @@ public class Cottonly {
     public Cottonly() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(CottonlyClient::doClientStuff);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CottonConfig.COMMON_CONFIG);
+        /*ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CottonConfig.CLIENT_CONFIG);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, CottonConfig.SERVER_CONFIG);*/
         registeryHandler = new RegisterHandler();
+        /*MinecraftForge.EVENT_BUS.addListener(LootHandler::loadLoot);*/
     }
 
     public static RegisterHandler getRegisteryHandler() {
