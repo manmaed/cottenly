a4693ccb262ad5133984eb6074e5c74460f6f525
manmaed <manmaed18@gmail.com>
2021-12-05 13:02:02 +0000

    Cotton Seed now shows in tabs :D

--- src/main/java/net/manmaed/cottonly/blocks/CottonPlant.java
+++ src/main/java/net/manmaed/cottonly/blocks/CottonPlant.java
@@ -2,6 +2,9 @@ package net.manmaed.cottonly.blocks;
 
 import net.manmaed.cottonly.items.CItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.CropBlock;
@@ -16,12 +19,17 @@ public class CottonPlant extends CropBlock {
     }
 
     @Override
-    protected ItemLike getBaseSeedId() {
+    public ItemLike getBaseSeedId() {
         return CItems.COTTON_SEED.get();
     }
 
     @Override
     public PlantType getPlantType(BlockGetter world, BlockPos pos) {
-        return super.getPlantType(world, pos);
+        return PlantType.CROP;
+    }
+
+    @Override
+    public void fillItemCategory(CreativeModeTab tab, NonNullList<ItemStack> itemStack) {
+        itemStack.add(new ItemStack(CItems.COTTON_SEED.get()));
     }
 }
