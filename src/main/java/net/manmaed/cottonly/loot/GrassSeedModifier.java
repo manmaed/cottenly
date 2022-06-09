efdb0f0984c85a33269826558ee867ac3eab7d29
manmaed <manmaed18@gmail.com>
2022-06-09 15:49:31 +0100

    fix getRegistryName

--- src/main/java/net/manmaed/cottonly/loot/GrassSeedModifier.java
+++ src/main/java/net/manmaed/cottonly/loot/GrassSeedModifier.java
@@ -1,6 +1,7 @@
 package net.manmaed.cottonly.loot;
 
 import com.google.gson.JsonObject;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.manmaed.cottonly.items.CItems;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -8,6 +9,7 @@ import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -22,9 +24,8 @@ public class GrassSeedModifier extends LootModifier {
         super(conditionsIn);
     }
 
-    @NotNull
     @Override
-    protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
+    protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
         generatedLoot.add(new ItemStack(CItems.COTTON_SEED.get()));
         return generatedLoot;
     }
@@ -39,7 +40,8 @@ public class GrassSeedModifier extends LootModifier {
         @Override
         public JsonObject write(GrassSeedModifier instance) {
             JsonObject jsonObject = makeConditions(instance.conditions);
-            jsonObject.addProperty("item", CItems.COTTON_SEED.get().getRegistryName().toString());
+            ResourceLocation location = ForgeRegistries.ITEMS.getKey(CItems.COTTON_SEED.get());
+            jsonObject.addProperty("item", location.toString());
             return jsonObject;
         }
     }
